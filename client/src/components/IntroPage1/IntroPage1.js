a8c2ca52a5fc2736049eee521a998e2a1d5e2725
Matt Hill <matt@harness.io>
2019-06-01 10:06:42 -0700

    asdf

--- client/src/components/IntroPage1/IntroPage1.js
+++ client/src/components/IntroPage1/IntroPage1.js
@@ -4,8 +4,18 @@ import _get from "lodash.get";
 
 import Images from "../../images/images.js";
 
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+// import Button from "@material-ui/core/Button";
+// import TextField from "@material-ui/core/TextField";
+
+import {
+  TextField,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  OutlinedInput,
+  MenuItem
+} from "@material-ui/core";
 
 import css from "./IntroPage1.module.scss";
 
@@ -27,7 +37,49 @@ class IntroPage1 extends React.Component {
     this.setState({ showYou: true });
   };
 
+  useStyles = () => {
+    return {};
+  };
+
+  // useStyles = makeStyles(theme => ({
+  //   root: {
+  //     display: "flex",
+  //     flexWrap: "wrap"
+  //   },
+  //   formControl: {
+  //     margin: theme.spacing(1),
+  //     minWidth: 120
+  //   },
+  //   selectEmpty: {
+  //     marginTop: theme.spacing(2)
+  //   }
+  // }));
+
   renderIntroText = () => {
+    const { showYou } = this.state;
+    const inputLabel = "test";
+    const classes = "test";
+    const values = { age: 5 };
+
+    // const classes = this.useStyles();
+    // const [values, setValues] = React.useState({
+    //   age: "",
+    //   name: "hai"
+    // });
+
+    // const inputLabel = React.useRef(null);
+    // const [labelWidth, setLabelWidth] = React.useState(0);
+    // React.useEffect(() => {
+    //   setLabelWidth(inputLabel.current.offsetWidth);
+    // }, []);
+
+    // function handleChange(event) {
+    //   setValues(oldValues => ({
+    //     ...oldValues,
+    //     [event.target.name]: event.target.value
+    //   }));
+    // }
+
     return (
       <div className={css.introText}>
         <div className={css.content}>hello</div>
@@ -43,14 +95,43 @@ class IntroPage1 extends React.Component {
           variant="outlined"
         />
         <div className={css.content}>you are a...</div>
-        <Button onClick={this.showYou} variant="contained" color="primary">
-          CLICK
-        </Button>
+
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel ref={inputLabel} htmlFor="outlined-age-simple">
+            Age
+          </InputLabel>
+          <Select
+            value={values.age}
+            // onChange={handleChange}
+            input={
+              <OutlinedInput
+                // labelWidth={labelWidth}
+                name="age"
+                id="outlined-age-simple"
+              />
+            }
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>GIRL</MenuItem>
+            <MenuItem value={20}>CAT</MenuItem>
+            <MenuItem value={30}>UNICORN</MenuItem>
+          </Select>
+        </FormControl>
+        {showYou && <span>GIRL</span>}
+        {!showYou && (
+          <Button onClick={this.showYou} variant="contained" color="primary">
+            CLICK
+          </Button>
+        )}
       </div>
     );
   };
+
   render() {
     const { params } = this.props;
+    const { showYou } = this.state;
 
     const youImage = "unicorn";
     const backgroundImage2 = "forestRight";
@@ -73,11 +154,13 @@ class IntroPage1 extends React.Component {
         </div>
         {this.renderIntroText()}
         <div className={css.characters}>
-          <img
-            className={`${css.characterImage} ${css.character1}`}
-            src={Images[friendImage]}
-            alt={friendImage}
-          />
+          {showYou && (
+            <img
+              className={`${css.characterImage} ${css.character1}`}
+              src={Images[friendImage]}
+              alt={friendImage}
+            />
+          )}
           <img
             className={`${css.characterImage} ${css.character1}`}
             src={Images[youImage]}
