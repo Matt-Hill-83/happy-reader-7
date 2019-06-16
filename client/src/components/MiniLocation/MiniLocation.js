cbeed2dd3d27429f5565045795e44852112c322b
Matt Hill <matt@harness.io>
2019-06-15 22:47:43 -0700

    asdf

--- client/src/components/MiniLocation/MiniLocation.js
+++ client/src/components/MiniLocation/MiniLocation.js
@@ -5,47 +5,59 @@ import Images from "../../images/images.js";
 
 import css from "./MiniLocation.module.scss";
 
-const MiniLocation = props => {
-  const { location, characters, isActive, className, showLabel = true } = props;
+class MiniLocation extends React.Component {
+  render() {
+    const {
+      location,
+      characters,
+      isActive,
+      className,
+      showLabel = true
+    } = this.props;
 
-  const localClass = isActive ? css.activeClass : "";
-  const miniLocation = Images.locations.small[location];
+    const localClass = isActive ? css.activeClass : "";
+    console.log("isActive", isActive); // zzz
 
-  const rockImage = Images.backgrounds["rock"];
+    const miniLocation = Images.locations.small[location];
 
-  return (
-    <div className={`${css.main} ${className} ${localClass}`}>
-      <div className={css.rockImage}>
-        <img className={css.rockImage} src={rockImage} alt={"imagex"} />
-      </div>
-      <div className={css.grassImage} />
-      <div className={css.topRow}>
-        <button className={css.topDoor} />
-      </div>
-      <div className={css.midRow}>
-        <div className={css.leftCol}>
-          <button className={css.leftSideDoor} />
+    const rockImage = Images.backgrounds["rock"];
+
+    return (
+      <div className={`${css.main} ${className} ${localClass}`}>
+        <div className={css.rockImage}>
+          <img className={css.rockImage} src={rockImage} alt={"imagex"} />
         </div>
-        <div className={css.midCol}>
-          <div className={css.imagesBox}>
-            <div className={css.characters}>{characters}</div>
-            <img
-              className={css.miniLocationImage}
-              src={miniLocation}
-              alt={"imagex"}
-            />
-            {showLabel && <span className={css.locationTitle}>{location}</span>}
+        <div className={css.grassImage} />
+        <div className={css.topRow}>
+          <button className={"top-door"} />
+        </div>
+        <div className={css.midRow}>
+          <div className={css.leftCol}>
+            <button className={"left-side-door"} />
+          </div>
+          <div className={css.midCol}>
+            <div className={css.imagesBox}>
+              <div className={css.characters}>{characters}</div>
+              <img
+                className={css.miniLocationImage}
+                src={miniLocation}
+                alt={"imagex"}
+              />
+              {showLabel && (
+                <span className={css.locationTitle}>{location}</span>
+              )}
+            </div>
+          </div>
+          <div className={css.rightCol}>
+            <button className={"right-side-door"} />
           </div>
         </div>
-        <div className={css.rightCol}>
-          <button className={css.rightSideDoor} />
+        <div className={css.bottomRow}>
+          <button className={"bottom-door"} />
         </div>
       </div>
-      <div className={css.bottomRow}>
-        <button className={css.bottomDoor} />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default observer(MiniLocation);
