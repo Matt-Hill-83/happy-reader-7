e062da09f636da55ba598f62677895f15f0003d6
Matt Hill <matt@harness.io>
2019-07-02 23:28:46 -0700

    safd

--- client/src/components/FrameBuilder/FrameBuilder.js
+++ client/src/components/FrameBuilder/FrameBuilder.js
@@ -18,24 +18,14 @@ class FrameBuilder extends Component {
     this.setInitialFrames()
   }
 
+  componentDidMount() {
+    this.setInitialFrames()
+  }
+
   setInitialFrames = () => {
-    const {
-      sceneToEdit: { frames, creatures = [] }
-    } = this.props
+    const frameSets = this.getFrameSets()
 
-    let newFrames = [
-      {
-        creatures: ["liz"],
-        story: ["the girl talks"],
-        dialog: [{ character: "default", text: "Oh my gosh I can talk!" }]
-      }
-    ]
-
-    if (frames) {
-      newFrames = frames
-    }
-
-    this.setState({ frames: newFrames })
+    this.setState({ frameSets })
   }
 
   onExitFrameBuilder = () => {
@@ -46,16 +36,12 @@ class FrameBuilder extends Component {
   }
 
   addFrameSet = ({}) => {
-    // addFrameSet = ({ frameSet = { name: 5 } }) => {
-
-    const frameSet = { name: "test", frames: this.test }
+    const frameSet = { name: "small 2", frames: this.test }
     frameSetStore.add(frameSet)
   }
 
   getFrameSets = () => {
-    return frameSetStore.docs.map(frameSet =>
-      toJS(frameSet.data && frameSet.data)
-    )
+    return frameSetStore.docs.map(frameSet => frameSet.data && frameSet.data)
   }
 
   renderFrameSets = () => {
@@ -67,7 +53,7 @@ class FrameBuilder extends Component {
   }
 
   render() {
-    const frameSets = this.getFrameSets()
+    // const frameSets = this.getFrameSets()
 
     const {
       sceneToEdit,
@@ -83,45 +69,62 @@ class FrameBuilder extends Component {
           { character: "kat", face: "cry" }
         ],
         dialog: [
-          { character: "liz", text: "Liz! Liz!!" },
-          { character: "kat", text: "Hi Kat." },
-          { character: "liz", text: "Do you want to play?" },
-          { character: "kat", text: "No, I can not play." },
-          { character: "kat", text: "I lost Piggy!." },
-          { character: "liz", text: "You lost Piggy?" },
-          { character: "liz", text: "Nooooooooo!" }
-        ]
-      },
-      {
-        creatures,
-        story: [`Liz lost Piggy.`, `Kat is sad.`],
-        faces: [
-          { character: "liz", face: "scared" },
-          { character: "kat", face: "cry" }
-        ],
-        dialog: [
-          { character: "kat", text: "oh noooo!" },
-          { character: "liz", text: "I can help!" }
-        ]
-      },
-      {
-        creatures,
-        story: [`Liz finds Piggy.`, `Liz is happy!`],
-        faces: [
-          { character: "kat", face: "joy" },
-          { character: "liz", face: "joy" }
-        ],
-        dialog: [
-          { character: "kat", text: "I love you Liz!" },
-          { character: "liz", text: "I love you Kat!" }
+          { character: "liz", text: "TEST" },
+          { character: "kat", text: "TEST" }
         ]
       }
     ]
+
+    // const frames = [
+    //   {
+    //     creatures,
+    //     story: [`Kat meets Liz.`, `Kat and Liz play.`],
+    //     faces: [
+    //       { character: "liz", face: "scared" },
+    //       { character: "kat", face: "cry" }
+    //     ],
+    //     dialog: [
+    //       { character: "liz", text: "Liz! Liz!!" },
+    //       { character: "kat", text: "Hi Kat." },
+    //       { character: "liz", text: "Do you want to play?" },
+    //       { character: "kat", text: "No, I can not play." },
+    //       { character: "kat", text: "I lost Piggy!." },
+    //       { character: "liz", text: "You lost Piggy?" },
+    //       { character: "liz", text: "Nooooooooo!" }
+    //     ]
+    //   },
+    //   {
+    //     creatures,
+    //     story: [`Liz lost Piggy.`, `Kat is sad.`],
+    //     faces: [
+    //       { character: "liz", face: "scared" },
+    //       { character: "kat", face: "cry" }
+    //     ],
+    //     dialog: [
+    //       { character: "kat", text: "oh noooo!" },
+    //       { character: "liz", text: "I can help!" }
+    //     ]
+    //   },
+    //   {
+    //     creatures,
+    //     story: [`Liz finds Piggy.`, `Liz is happy!`],
+    //     faces: [
+    //       { character: "kat", face: "joy" },
+    //       { character: "liz", face: "joy" }
+    //     ],
+    //     dialog: [
+    //       { character: "kat", text: "I love you Liz!" },
+    //       { character: "liz", text: "I love you Kat!" }
+    //     ]
+    //   }
+    // ]
 
     this.test = frames
     const renderedFrames = frames.map(frame => {
       return <Frame frame={frame} sceneToEdit={sceneToEdit} />
     })
+
+    console.log("this.state.frameSets", this.state.frameSets) // zzz
 
     return (
       <div className={css.main}>
