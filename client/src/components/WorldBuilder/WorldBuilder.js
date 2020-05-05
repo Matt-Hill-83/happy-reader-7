14d7cde3f42a8a409b369ec391f39bcd091dc3cd
Matt Hill <matt@harness.io>
2020-05-05 00:26:13 -0700

    asdf

--- client/src/components/WorldBuilder/WorldBuilder.js
+++ client/src/components/WorldBuilder/WorldBuilder.js
@@ -30,7 +30,7 @@ const INITIAL_MAP_INDEX = 0
 // const NUM_ROWS_LOCATIONS_GRID = 2
 // const NUM_COLS_LOCATIONS_GRID = 3
 const NUM_ROWS_LOCATIONS_GRID = 5
-const NUM_COLS_LOCATIONS_GRID = 20
+const NUM_COLS_LOCATIONS_GRID = 7
 
 class WorldBuilder extends Component {
   state = {
