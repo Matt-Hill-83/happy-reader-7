1b4bfb5509d6028caca78177ad4bfb18c151c1f2
Matt Hill <matt@harness.io>
2020-05-25 04:47:30 -0700

    added 011-LizIsSlow

--- client/src/Scripts/011-LizIsSlow.js
+++ client/src/Scripts/011-LizIsSlow.js
@@ -1,11 +1,11 @@
 const story011 = {
-  title: "011-LizIsSlow",
+  title: "011 - Liz is A Slow Poke",
   description: [
     "1: some stuff----------------",
     "2: some stuff----------------",
   ],
   scenes: {
-    cave: {
+    swing: {
       sceneConfig: {
         coordinates: { col: 1, row: 1 },
         creatures: ["kat", "liz2"],
@@ -23,11 +23,26 @@ const story011 = {
             creatures: ["kat", "liz2"],
           },
           dialogs: [
-            `{"liz2": "Put some pep in your step."}`,
+            `{"kat": "Liz, put some pep in your step."}`,
             `{"liz2": "What?"}`,
-            `{"liz2": "You are so sloooowwwww!!!!"}`,
-            `{"liz2": "What?, I'm not slow."}`,
-            `{"kat": "You dawdle."}`,
+            `{"kat": "Put some click in your clogs."}`,
+            `{"liz2": "I don't have clogs."}`,
+          ],
+        },
+        {
+          frameConfig: {
+            faces: [
+              { face: "happy", character: "liz2" },
+              { character: "kat", face: "kat-smiling.49647334.png" },
+            ],
+            creatures: ["kat", "liz2"],
+          },
+          dialogs: [
+            `{"kat": "You are sooooooooo sloooowwwww!!!!"}`,
+            `{"liz2": "I am not slow."}`,
+            `{"kat": "Liz, you are slower than a snail taped on top of a turtle."}`,
+            `{"liz2": "Am not!"}`,
+            `{"kat": "Liz, you are my best friend, but you are a dawdler."}`,
           ],
         },
         {
@@ -36,19 +51,59 @@ const story011 = {
               { character: "liz2", face: "happy" },
               { character: "kat", face: "kat-cringing.62a27ad4.png" },
             ],
-            creatures: ["kat", "liz2", "troll01", "troll02"],
+            creatures: ["kat", "liz2"],
           },
           dialogs: [
-            `{"liz2": "Um... Siri, what the heck is my friend talking about?"}`,
-            `{"kat": "Liz, you are slower than a snail taped on top of a turtle."}`,
+            `{"liz2": "I have no idea what you are talking about."}`,
+            `{"liz2": "Um... Siri, what on earth is my friend talking about?"}`,
+            `{"liz2": "Make her words make sense."}`,
             `{"siri": "Dawdler:  someone who dilly dallies, also: a slow-poke. "}`,
-            `{"kat": "Ha. See, even siri thinks you are slow."}`,
+          ],
+        },
+        {
+          frameConfig: {
+            faces: [
+              { character: "liz2", face: "happy" },
+              { character: "kat", face: "kat-cringing.62a27ad4.png" },
+            ],
+            creatures: ["kat", "liz2"],
+          },
+          dialogs: [
+            `{"kat": "Ha! See, even siri thinks you are slow."}`,
             `{"liz2": "Aaaugh!!! , Siri! Take that back!"}`,
+          ],
+        },
+        {
+          frameConfig: {
+            faces: [
+              { character: "liz2", face: "happy" },
+              { character: "kat", face: "kat-cringing.62a27ad4.png" },
+            ],
+            creatures: ["kat", "liz2"],
+          },
+          dialogs: [
+            `{"liz2": "Siri!  That was mean!"}`,
+            `{"liz2": "Take that back this instant!"}`,
+            `{"siri": "Re-calculating... "}`,
+          ],
+        },
+        {
+          frameConfig: {
+            faces: [
+              { character: "liz2", face: "happy" },
+              { character: "kat", face: "kat-cringing.62a27ad4.png" },
+            ],
+            creatures: ["kat", "liz2"],
+          },
+          dialogs: [
+            `{"siri": "Did you want me to play: Take that back this instant - by the Backstreet Bugs? "}`,
+            `{"kat": "The Backstreet Bugs! "}`,
+            `{"liz2": "I love The Backstreet Bugs! "}`,
           ],
         },
       ],
     },
-    bees: {
+    lake: {
       sceneConfig: {
         coordinates: { col: 1, row: 2 },
         creatures: ["kat", "liz2"],
@@ -66,52 +121,11 @@ const story011 = {
             creatures: ["kat", "liz2"],
           },
           dialogs: [
-            `{"liz2": "Um... Siri, what the heck is my friend talking about?"}`,
-            `{"kat": "Liz, you are slower than a snail taped on top of a turtle."}`,
-            `{"siri": "Dawdler:  someone who dilly dallies, also: a slow-poke. "}`,
-            `{"kat": "Ha. See, even siri thinks you are slow."}`,
-            `{"liz2": "Aaaugh!!! , Siri! Take that back!"}`,
-          ],
-        },
-        {
-          frameConfig: {
-            faces: [
-              { character: "liz2", face: "happy" },
-              { character: "kat", face: "kat-cringing.62a27ad4.png" },
-            ],
-            creatures: ["kat", "liz2", "troll01", "troll02"],
-          },
-          dialogs: [
-            `{"liz2": "Take that back this instant!"}`,
-            `{"siri": "Did you want me to play: Take that back this instant - by the Backstreet Bugs? "}`,
-            `{"siri": "I love the The backstreet bugs! "}`,
-          ],
-        },
-      ],
-    },
-    lake: {
-      sceneConfig: {
-        coordinates: { col: 1, row: 3 },
-        creatures: ["kat", "liz2"],
-        isEndScene: false,
-        isStartScene: true,
-        items: [{ name: "empty" }],
-      },
-      frames: [
-        {
-          frameConfig: {
-            faces: [
-              { face: "happy", character: "liz2" },
-              { character: "kat", face: "kat-smiling.49647334.png" },
-            ],
-            creatures: ["kat", "liz2"],
-          },
-          dialogs: [
-            `{"katie": "Summer Time is the funnest time"}`,
-            `{"katie": "Summer Time is the funnest time"}`,
-            `{"katie": "Summer Time is the funnest time"}`,
-            `{"katie": "Summer Time is Fun!"}`,
-            `{"katie": "Okay fine, catch up with me later."}`,
+            `{"kat": "Summer Time is the funnest time"}`,
+            `{"liz2": "Summer Time is the funnest time"}`,
+            `{"kat": "Summer Time is the funnest time"}`,
+            `{"kat": "Summer Time is Fun!"}`,
+            `{"kat": "Okay fine, catch up with me later."}`,
           ],
         },
       ],
