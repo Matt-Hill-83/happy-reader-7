fbff4583616e08617c497859dd77b762d943b5cf
Matt Hill <matt@harness.io>
2020-05-26 01:20:33 -0700

    asdf

--- client/src/components/FrameSetUploader/FrameSetUploader.js
+++ client/src/components/FrameSetUploader/FrameSetUploader.js
@@ -28,8 +28,8 @@ class FrameSetUploader extends Component {
   // state = { text: JSON.stringify(story005) }
   // state = { text: JSON.stringify(story011) }
   // state = { text: JSON.stringify(story200) }
-  // state = { text: JSON.stringify(story004) }
-  state = { text: JSON.stringify(story015) }
+  state = { text: JSON.stringify(story004) }
+  // state = { text: JSON.stringify(story015) }
   // state = { text: JSON.stringify(story010) }
   // state = { text: JSON.stringify(story013) }
   // state = { text: JSON.stringify(story014) }
