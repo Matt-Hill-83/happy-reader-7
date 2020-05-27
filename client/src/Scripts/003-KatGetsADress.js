ef983adf42d366b2cd562a73da37681412cee6ef
Matt Hill <matt@harness.io>
2020-05-27 02:00:00 -0700

    new stories - 002, 003

--- client/src/Scripts/003-KatGetsADress.js
+++ client/src/Scripts/003-KatGetsADress.js
@@ -1,5 +1,5 @@
-const template = {
-  title: "title",
+const story003 = {
+  title: "003 - Kat Gets A Dress",
   description: [
     "1: some stuff----------------",
     "2: some stuff----------------",
@@ -31,8 +31,35 @@ const template = {
             creatures: ["kat", "liz2"],
           },
           dialogs: [
-            '{"liz2" : "-----------------------------------------------"}',
-            '{"kat" : "++++++++++"}',
+            '{"kat" : "I got my dress!"}',
+            '{"liz2" : "Kat, have you lost your marbles?"}',
+            '{"kat" : "I got my dress!!!"}',
+            '{"kat" : "I got my dress!!!"}',
+            '{"kat" : "I got my dress!!!"}',
+            '{"liz2" : "Wait, you really got a dress?"}',
+            '{"liz2" : "You really, actually got a dress?"}',
+            '{"kat" : "I did it, I did it!"}',
+            '{"liz2" : "Like... a \'dress\' dress?"}',
+            // '{"kat" : "Yes! Yes!"}',
+            '{"kat" : "Yes! a \'dress\' dress"}',
+            '{"kat" : "Oh liz, Oh liz, I need to sing!"}',
+            '{"liz2" : "Put A Cat A"}',
+            '{"liz2" : "Put A Cat A"}',
+            '{"liz2" : "Put A Cat A"}',
+            '{"kat" : "I just got my dress!"}',
+            '{"kat" : "This dress is the best!"}',
+            '{"kat" : "And I finished up my quest!"}',
+            '{"kat" : "It was my best Dress Quest quest yet!"}',
+            '{"kat" : "That quest was the best!"}',
+            '{"troll01" : "I\'m impressed."}',
+            '{"kat" : "And now I\'ve got a teeny tiny question..."}',
+            '{"kat" : "...it\'s like a little test."}',
+            '{"kat" : "Who\'s gonna be the best dressed?"}',
+            '{"kat" : "At the ball?"}',
+            '{"kat" : "Who\'s gonna be the best dressed guest"}',
+            '{"kat" : "Of them all?"}',
+            '{"liz2" : "Kat, I\'m enthralled.  Now you can slay it with the best."}',
+            // '{"liz2" : "Kat, is that dress for a doll?"}',
           ],
         },
         {
@@ -93,4 +120,4 @@ const template = {
   },
 }
 
-export default template
+export default story003
